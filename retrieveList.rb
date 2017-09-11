c6051d76a32cd8ceb71ef993b601be0f50470e75
Douglas Lovell <doug@wbreeze.com>
2017-09-11 16:31:29 -0300

    Get to the download

--- retrieveList.rb
+++ retrieveList.rb
@@ -12,22 +12,42 @@ class Golden
   def retriever()
     url = URI.parse(@config['url'])
     root = url.scheme + '://' + url.host
-    Capybara.current_driver = :selenium
-    Capybara.app_host = root
+    Capybara.configure do |cap|
+      cap.current_driver = :selenium
+      cap.app_host = root
+      cap.default_max_wait_time = 30 # seconds
+    end
 
     puts "Visiting #{url.path} at #{root}"
     visit(url.path)
 
-    puts "Signing-in..."
+    puts 'Signing-in...'
     fill_in('username', with: @config['uname'])
     fill_in('password', with: @config['pwd'])
     select(@config['dom'], from: 'domain')
     click_button('loginButton')
 
-    link = find(:xpath, '//a[text="Partner upload/download area"]')
-    puts "Navigating to download area"
-    save_and_open_page
-    link.click
+    puts 'Navigating to download area'
+    link = find(:xpath, '//a[text()="Partner upload/download area"]')
+    popup = window_opened_by do
+      link.click
+    end
+
+    puts "Selecting #{@config['filename']}"
+    within_window(popup) do
+      test = "text()=\"#{@config['filename']}\""
+      puts "TEST #{test}"
+      file_cell = find(:xpath, "//td[#{test}]")
+      file_cell.click
+
+      puts 'Selecting the download action'
+      download_cell = find(:xpath, '//table[@id="fileMenu"]//td/font[contains(text(),"Download")]')
+      download_cell.click
+
+      puts 'Allowing download'
+      # i have a modal here
+      sleep(30)
+    end
   end
 end
 
